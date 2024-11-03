831d5a7c6f06f89ef717006e39eb2060728f326b
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-11-03 15:47:16 +0530

    practise: updating an cart using state hook objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
-import NavBar from "./components/Cart/NavBar";
-import Cart from "./components/Cart/Cart";
 
 function App() {
-  const [cart, setCart] = useState(["product1", "product2"]);
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
-  return <div>
-    <NavBar cartCount = {cart.length}/>
-    <Cart cartItems = {cart} onClear={() => setCart([])}/>
-  </div>;
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        (item.id === 1) ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  };
+  
+
+  return (
+    <div>
+      <ul>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            {item.id} {item.title} {item.quantity}{" "}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>update</button>
+    </div>
+  );
 }
 
 export default App;

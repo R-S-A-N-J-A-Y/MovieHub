8840fce251c362f7988bbbfaed451ce2e860dd7d
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-11-03 17:22:38 +0530

    Practise: updating an form from useRef to useState hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,53 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: '',
+  });
 
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
 
-    const person = {name: '', age: 0};
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="nane" className="form-label">
+          Name
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
 
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        if (nameRef.current !== null)
-            person.name = nameRef.current.value;
-        if (ageRef.current !== null)
-            person.age = parseInt(ageRef.current.value);
-        console.log(person);
-    }
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
 
-    return (
-        <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-            <label htmlFor="nane" className="form-label">Name</label>
-            <input ref={nameRef} id="name" type="text" className="form-control" />
-        </div>
-
-        <div className="mb-3">
-            <label htmlFor="age" className="form-label">Age</label>
-            <input ref={ageRef} id="age" type="number" className="form-control" />
-        </div>
-
-        <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    );
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Form;
